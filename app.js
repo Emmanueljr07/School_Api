902321f9028320f4d38bd7d58d5b67d79d6e7224
Emmanueljr07 <116388126+Emmanueljr07@users.noreply.github.com>
2024-07-31 10:06:37 +0100

    step-5

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require("express");
 const body_parser = require("body-parser");
 const userRouter = require("./router/user-routes");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
+const options = {
+  origin: process.env.FRONT_END_URL,
+};
 
 const app = express();
+app.use(cors(options));
 
 app.use(body_parser.json());
 
